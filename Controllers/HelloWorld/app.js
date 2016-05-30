1c4c0b6f26b2d6fe474a3d065fab874cfe4c2a11
gruprog <gruprog@gmail.com>
2016-05-30 16:01:45 -0400

    controller adding function to the scope

--- Controllers/HelloWorld/app.js
+++ Controllers/HelloWorld/app.js
@@ -1,3 +1,12 @@
 angular.module('app', []).controller('WishingController', function($scope) {
   $scope.message = 'Hello World!';
+
+  $scope.greetings = ['Welcome!', 'Good Morning!', 'Good Day!'];
+  $scope.initialGreeting = $scope.greetings[0];
+
+  $scope.greet = function() {
+    var index = parseInt(Math.random() * $scope.greetings.length);
+
+    $scope.initialGreeting = $scope.greetings[index];
+  }
 });
